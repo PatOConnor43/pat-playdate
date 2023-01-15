c79281857c71fce1e30918b61f8d4fd21c5086cc
Pat O'Connor <patoconnor43@gmail.com>
2023-01-15 01:15:45 -0700

    Demo handling buttons

--- source/main.lua
+++ source/main.lua
@@ -3,12 +3,30 @@ import 'CoreLibs/graphics'
 
 local gfx = playdate.graphics
 
-gfx.setColor(gfx.kColorBlack)
 local crankValue = 0
 local counter = 0
 local radius = 250
 local transformer = function (r)
     return r + 3
+end
+local pos_x = 200
+local pos_y = 120
+
+local function handle_buttons(p_x, p_y)
+    if playdate.buttonIsPressed(playdate.kButtonLeft) then
+        p_x = (p_x - 5) % 400
+    end
+    if playdate.buttonIsPressed(playdate.kButtonRight) then
+        p_x = (p_x + 5) % 400
+    end
+    if playdate.buttonIsPressed(playdate.kButtonUp) then
+        p_y = (p_y - 5) % 240
+    end
+    if playdate.buttonIsPressed(playdate.kButtonDown) then
+        p_y = (p_y + 5) % 240
+    end
+
+    return p_x, p_y
 end
 
 function playdate.update()
@@ -27,7 +45,8 @@ function playdate.update()
             return r - 3
         end
     end
-    gfx.fillCircleAtPoint(200, 120, radius)
+    pos_x, pos_y = handle_buttons(pos_x, pos_y)
+    gfx.fillCircleAtPoint(pos_x, pos_y, 10)
     gfx.setColor(gfx.kColorBlack)
 end
 
